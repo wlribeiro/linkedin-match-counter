f87199fe3ae425c7d1388dd94b91966f12be924d
William <rybeyro93@gmail.com>
2022-03-08 14:49:23 -0300

    add new methods to run scripts

--- src/background.js
+++ src/background.js
@@ -1,11 +1,38 @@
+var tabUrl;
+
 chrome.tabs.onActivated.addListener( (tab) => {
     getTabAndExecScript(tab)
 });
 
+// chrome.tabs.onUpdated.addListener(tab => {
+//     getCurrentTab();
+//     if(tabUrl){
+//         getTabAndExecScript(tab)
+//     }
+// });
+
+
+async function getCurrentTab() {
+    let queryOptions = {
+        active: true,
+        currentWindow: true
+    };
+
+    let [tab] = await chrome.tabs.query(queryOptions)
+    tabUrl = tab.url;
+}
+
 function getTabAndExecScript(tab){
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        if(/^https:\/\/www\.linkedin/.test(current_tab_info.url)){
-            executeScript(tab.tabId)
+    let id;
+    
+    if(typeof(tab) === 'number'){
+        id = tab
+    } else {
+        id = tab.tabId
+    }
+    chrome.tabs.get(id, current_tab_info => {
+        if(/^https:\/\/www\.linkedin\/talent\/profile/.test(current_tab_info.url)){
+            executeScript(id)
         }
     });
 
